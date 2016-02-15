7b3840eb0cf92a6a3630fe3ef2820138027c304c
Marox44 <marek.lamasz@gmail.com>
2016-02-15 17:42:29 +0100

    Initial re-commit. First version of installer | v1.0 | :v:

---
+++ Brightness Indicator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Brightness_Indicator
+{
+    static class Program
+    {
+        static Mutex mutex = new Mutex(true, "{brightness-indicator-mutex}");
+        [STAThread]
+        static void Main()
+        {
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                Marox.Alert.Error("Another instance is already running!");
+            }
+        }
+    }
+}
---
+++ Brightness Indicator/Volume.cs
@@ -0,0 +1,41 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+
+namespace Brightness_Indicator
+{
+    internal class Volume
+    {
+        private static MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+        private static MMDevice device = DevEnum.GetDefaultAudioEndpoint((DataFlow)0, (Role)1);
+
+        public static int GetVolume()
+        {
+            int result = 100;
+            try
+            {
+                result = (int)(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            }
+            catch (Exception)
+            {
+            }
+
+            return result;
+        }
+
+        public static bool GetMute()
+        {
+            bool result = false;
+            try
+            {
+                // MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+                //MMDevice device = DevEnum.GetDefaultAudioEndpoint((DataFlow)0, (Role)1);
+                result = device.AudioEndpointVolume.Mute;
+            }
+            catch (Exception)
+            {
+            }
+
+            return result;
+        }
+    }
+}
